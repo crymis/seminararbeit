89d23a1e2e981bfd10715c7e321ba7d10248f909
Johannes Reuter <jre@alpha-q.de>
2014-04-23 21:15:34 +0200

    svg-technologie-kapitel geschrieben

---
+++ assets/svg_example.js
@@ -0,0 +1,7 @@
+//erzeuge svg-root-element
+var cEl = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+//erzeuge svg-circle-element
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+cEl.appendChild(circle);
+
