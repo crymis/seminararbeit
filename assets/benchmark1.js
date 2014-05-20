3fbd620b87c3d88ffc30ae08fc4bcaa79ef96c75
Johannes Reuter <jre@alpha-q.de>
2014-05-20 11:43:19 +0200

    detail-anpassungen ausarbeitung

--- assets/benchmark1.js
+++ assets/benchmark1.js
@@ -1,30 +1,28 @@
 function simulation(drawingFunction) {
-        var objects = [];
+  var objects = [];
 	
-	//erzeuge objekte mit zufälliger position und geschwindigkeit
-	for(var i=0;i<amountObjects;i++) {
-		objects.push(getRandomObject());
-	}
+  //erzeuge objekte mit zufälliger position und geschwindigkeit
+  for(var i=0;i<amountObjects;i++) {
+	objects.push(getRandomObject());
+  }
 
-	//methode, die für jeden frame ausgeführt wird
-        function tick() {
-		for(var i=0;i<amountObjects;i++) { //für alle objekte
-			var objectToMove = objects[i];
-			//überprüfe auf kollision
-			if(objectToMove.isXCollision()) objectToMove.vecX *= -1;
-			if(objectToMove.isYCollision()) objectToMove.vecY *= -1;
-
-			//bewege objekt nach bewegungs-vektoren
-			objectToMove.move();
-		}
+  //methode, die für jeden frame ausgeführt wird
+  function tick() {
+      for(var i=0;i<amountObjects;i++) { //für alle objekte
+        var objectToMove = objects[i];
+		//überprüfe auf kollision
+		if(objectToMove.isXCollision()) objectToMove.vecX *= -1;
+		if(objectToMove.isYCollision()) objectToMove.vecY *= -1;
+		//bewege objekt nach bewegungs-vektoren
+		objectToMove.move();
+      }
 		
-		//zeichne objekte mit austauschbarer implementierung
-		drawingFunction(objects);
-        }
-
-	//starte game-loop
-        (function animloop(){
-          requestAnimFrame(animloop);
-          tick();
-        })();
-    }
+	//zeichne objekte mit austauschbarer implementierung
+	drawingFunction(objects);
+  }
+  //starte game-loop
+  (function animloop(){
+      requestAnimFrame(animloop);
+      tick();
+  })();
+}
